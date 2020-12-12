1d6730cf21fa1932348396d7738b106bb2a249d5
Eduardo Fernández León <eduardo.fernandez@geographica.com>
2020-12-12 20:42:53 +0100

    Fixing linter issues

--- src/api.py
+++ src/api.py
@@ -3,20 +3,23 @@ from metrics import metrics_to_text
 
 routes = web.RouteTableDef()
 
+
 def run_api(loop):
     """ Setup an AIOHTTP app and run it in a non-blocking mode """
     loop.create_task(
         _run_server()
     )
 
+
 async def _run_server():
     app = web.Application()
     app.add_routes(routes)
     runner = web.AppRunner(app)
     await runner.setup()
-    
-    site = web.TCPSite(runner, 'localhost', 8080) 
+
+    site = web.TCPSite(runner, 'localhost', 8080)
     await site.start()
+
 
 @routes.get('/metrics')
 async def get_metrics(request):
--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,3 @@
-import time
 import subprocess
 import json
 import asyncio
@@ -12,21 +11,30 @@ from prometheus_client import (
 METRICS = {}
 OUTPUT_FILE = 'outputfile.json'
 
+
 def run_worker(loop, collection, environment, time_to_wait):
     """ Setup a worker to run Newman CLI periodically """
-    print(f"Running Newman for collection {collection} every {time_to_wait} seconds")
-    
+    print(f"Running Newman for the collection {collection}")
+
     loop.create_task(
-        _run_periodically(time_to_wait, _generate_metrics, collection, environment)
+        _run_periodically(
+            time_to_wait,
+            _generate_metrics,
+            collection,
+            environment
+        )
     )
+
 
 def metrics_to_text():
     return generate_latest(REGISTRY).decode('utf-8')
+
 
 async def _run_periodically(wait_time, func, *args):
     while True:
         func(*args)
         await asyncio.sleep(wait_time)
+
 
 def _run_newman(collection, environment):
     _check_file(collection)
@@ -34,34 +42,40 @@ def _run_newman(collection, environment):
 
     environment = f'-e {environment}' if environment else ''
     process = subprocess.Popen(
-        [f'/usr/local/bin/newman run {collection} {environment} --reporters json --reporter-json-export {OUTPUT_FILE}'],
+        [f"""
+        /usr/local/bin/newman run {collection} {environment} \
+            --reporters json --reporter-json-export {OUTPUT_FILE}
+        """],
         shell=True, stdout=subprocess.PIPE)
     process.wait()
+
 
 def _generate_metrics(collection, environment):
     _run_newman(collection, environment)
     data = _read_file(OUTPUT_FILE)
     if not data:
-        print(f'Error ')
+        print('Error reading the newman output file')
         return
 
     for execution in data['run']['executions']:
         item = execution['item']
+        name = item['name']
         assertions = execution.get('assertions') or []
-        common_key = f"request_{item['name'].lower().replace(' ', '_')}"
-        
+        common_key = f"request_{_normalize(name)}"
+
         # Generating Gauge metrics for errors in tests
         errors = [e for e in assertions if e.get('error')]
         _create_gauge(
             name=f'{common_key}_errors',
-            description=f"Number of failed tests for the request {item['name']}",
+            description=f"Number of failed tests for the request {name}",
             value=len(errors)
         )
 
         for ass in assertions:
+            test_name = ass['assertion']
             _create_gauge(
-                name=f"{common_key}_{ass['assertion'].lower().replace(' ', '_')}",
-                description=f"Number of failure for the test '{ass['assertion']}'",
+                name=f"{common_key}_{_normalize(ass['assertion'])}",
+                description=f"Number of failure for the test '{test_name}'",
                 value=1 if 'error' in ass else 0
             )
 
@@ -78,6 +92,11 @@ def _generate_metrics(collection, environment):
             value=execution['response']['responseSize']
         )
 
+
+def _normalize(text):
+    return text.lower().replace(' ', '_')
+
+
 def _check_file(file):
     if file.startswith(('http://', 'https://')):
         return
@@ -85,15 +104,18 @@ def _check_file(file):
         print(f'File {file} not found')
         sys.exit(1)
 
+
 def _read_file(input_file):
     with open(input_file) as f:
         data = json.load(f)
     return data
 
+
 def _create_gauge(name, description, value):
     METRICS[name] = METRICS.get(name) or Gauge(name, description)
     gauge = METRICS.get(name)
     gauge.set(value)
+
 
 def _create_histogram(name, description, value):
     METRICS[name] = METRICS.get(name) or Histogram(name, description)
